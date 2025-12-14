fa8b48c12e57407fbeb49dbb583c590db772e412
VladTEA1 <Chaek2006@mail.ru>
2025-12-14 17:55:20 +0300

    Add first commit

---
+++ practicum.py
@@ -0,0 +1,27 @@
+# для активации venv
+
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+
+
+
+# print('Привет, Практикум!')
+
+
+
+
+
+# # Для Windows:
+# sou -> Tab -> пробел -> v -> Tab -> s -> Tab -> a -> Tab -> Enter
+
+
+
+
+
+# source venv/Scripts/activate
+
+
+
+
+
+def say_hello():
+    print('Привет, Практикум!')
